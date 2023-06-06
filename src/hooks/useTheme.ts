1ce33bc606b3134de86e25d540e780adf9c571ef
mzwine <i@mzwine.cc>
2023-06-06 17:42:58 +0800

    first commit

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,32 @@
+import { computed } from 'vue'
+import type { GlobalThemeOverrides } from 'naive-ui'
+import { darkTheme, useOsTheme } from 'naive-ui'
+import { useAppStore } from '@/store'
+
+export function useTheme() {
+  const appStore = useAppStore()
+
+  const OsTheme = useOsTheme()
+
+  const isDark = computed(() => {
+    if (appStore.theme === 'auto')
+      return OsTheme.value === 'dark'
+    else
+      return appStore.theme === 'dark'
+  })
+
+  const theme = computed(() => {
+    return isDark.value ? darkTheme : undefined
+  })
+
+  const themeOverrides = computed<GlobalThemeOverrides>(() => {
+    if (isDark.value) {
+      return {
+        common: {},
+      }
+    }
+    return {}
+  })
+
+  return { theme, themeOverrides }
+}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { setupStore } from '@/store'
+import { setupRouter } from '@/router'
+
+async function bootstrap() {
+  const app = createApp(App)
+
+  setupStore(app)
+
+  await setupRouter(app)
+
+  app.mount('#app')
+}
+
+bootstrap()
