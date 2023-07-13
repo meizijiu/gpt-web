9fca2b3808e6bb29ba5ed13e501f2c09b587bf72
mzwine <i@mzwine.cc>
2023-07-13 19:00:44 +0800

    自动引入naive-ui样式

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { setupAssets } from './plugins'
 import { setupStore } from './store'
 import { setupRouter } from './router'
 import { setupI18n } from './locales'
 
 async function bootstrap() {
   const app = createApp(App)
+
+  setupAssets()
 
   setupStore(app)
 
